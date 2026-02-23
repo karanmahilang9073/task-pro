7f22915ccfb51fb4ebb29f148b35b06c3d980003
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-02-23 14:06:06 +0530

    basic setup done

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+
+app.use(express.json())
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`server is running on port:${PORT}`)
+})
