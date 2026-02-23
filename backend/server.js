273159d6d98426c480521df949f1299c679a6d16
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-02-23 15:35:03 +0530

    global error handler created

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,13 @@ app.get('/', (req, res) =>  {
 //database call
 connectDB()
 
+//global error hander
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    console.log('error', err)
+    res.status(statusCode).json({success : false, message : err.message  || 'internal server error'})
+})
+
 app.listen(PORT, () => {
     console.log(`server is running on port:${PORT}`)
 })
