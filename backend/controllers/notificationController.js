3d7d0e0f4616ede63100c34ade97f24e084c5afd
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-03-02 22:11:27 +0530

    notification controlled implemented

---
+++ backend/controllers/notificationController.js
@@ -0,0 +1,51 @@
+import { asynchandler } from "../middleware/asynchandler.js";
+import Notification from "../models/Notification.js";
+
+
+export const getAllNotifications = asynchandler(async(req, res) => {
+    const userId = req.userId;
+    const notifications = await Notification.find({recipient : userId})
+    if(!notifications || notifications.length === 0){
+        const error = new Error('no notifications fount')
+        error.statusCode = 404
+        throw error
+    }
+    res.status(200).json({success : true, count : notifications.length, notifications : notifications})
+})
+
+export const getNotification = asynchandler(async(req, res) => {
+    const notificationId = req.params.id
+    const userId = req.userId
+    const notification = await Notification.findById(notificationId)
+    if(!notification){
+        const error = new Error('notification not found')
+        error.statusCode = 404
+        throw error
+    }
+    if(notification.recipient.toString() !== userId){
+        const error = new Error('you are not authorized to get notification access')
+        error.statusCode = 403
+        throw error
+    }
+    res.status(200).json({success : true, notification})
+})
+
+export const deleteNotification = asynchandler(async(req, res) => {
+    const userId = req.userId
+    const notificationId = req.params.id
+    const notification = await Notification.findById(notificationId)
+    if(!notification){
+        const error = new Error('notification not found')
+        error.statusCode = 404
+        throw error
+    }
+    if(notification.recipient.toString() !== userId){
+        const error = new Error('you are not authorized to delete this notification')
+        error.statusCode = 403
+        throw error
+    }
+
+    await Notification.findByIdAndDelete(notificationId)
+
+    res.status(200).json({success : true, message : 'notification deleted successfully'})
+})
