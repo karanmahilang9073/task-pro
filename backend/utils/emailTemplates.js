533961144355fdba54bdbfd5bd415b7f148acca6
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-03-04 17:32:29 +0530

    template structure maintained

--- backend/utils/emailTemplates.js
+++ backend/utils/emailTemplates.js
@@ -32,29 +32,29 @@ export const taskAssignmentTemplate = (title, description, deadline) => {
 export const deadlineReminderTemplate = (title,deadline,  daysLeft) => {
     return `
     <html>
-    <body>
-    <div class="container">
-      <div class="header">⚠️  task deadline approaching</div>
+        <body>
+            <div class="container">
+            <div class="header">⚠️  task deadline approaching</div>
 
-      <div class="task-box">
-        <div class="value">
-          <span class="label">Task Title:</span> ${title}
+            <div class="task-box">
+                <div class="value">
+                <span class="label">Task Title:</span> ${title}
+                </div>
+
+                <div class="value">
+                <span class="label">Deadline:</span> ${deadline}
+                </div>
+
+                <div class="value">
+                <span class="label">dayleft: </span> ${daysLeft}
+                </div>
+            </div>
+
+            <div class="footer">
+                Please make sure to complete the task before the deadline.<br/>
+                If you have any questions, contact the person who assigned it.
+            </div>
         </div>
-
-        <div class="value">
-          <span class="label">Deadline:</span> ${deadline}
-        </div>
-
-        <div class="value">
-          <span class="label">dayleft: </span> ${daysLeft}
-        </div>
-      </div>
-
-      <div class="footer">
-        Please make sure to complete the task before the deadline.<br/>
-        If you have any questions, contact the person who assigned it.
-      </div>
-    </div>
-  </body>
+    </body>
   </html>`
 }
