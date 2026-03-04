0ae6e253256d27045d4e115aa7baa25d4cd1a664
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-03-04 17:38:42 +0530

    .

--- backend/utils/emailTemplates.js
+++ backend/utils/emailTemplates.js
@@ -6,24 +6,24 @@ export const taskAssignmentTemplate = (title, description, deadline) => {
             <div class="container">
                 <div class="header">📌 New Task Assigned</div>
 
-                    <div class="task-box">
-                        <div class="value">
-                            <span class="label">Task Title:</span> ${title}
-                        </div>
-
-                        <div class="value">
-                            <span class="label">Description:</span><br/>
-                            ${description}
-                        </div>
-
-                        <div class="value">
-                            <span class="label">Deadline:</span> ${deadline}
-                        </div>
+                <div class="task-box">
+                    <div class="value">
+                        <span class="label">Task Title:</span> ${title}
                     </div>
 
-                    <div class="footer">
-                        Please make sure to complete the task before the deadline.<br/>
-                        If you have any questions, contact the person who assigned it.
+                    <div class="value">
+                        <span class="label">Description:</span><br/>
+                        ${description}
+                    </div>
+
+                    <div class="value">
+                        <span class="label">Deadline:</span> ${deadline}
+                    </div>
+                </div>
+
+                <div class="footer">
+                    Please make sure to complete the task before the deadline.<br/>
+                    If you have any questions, contact the person who assigned it.
                 </div>
             </div>
         </body>
