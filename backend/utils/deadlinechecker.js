0260ecaeba14c392acd04a844b682bc7226fe578
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-03-08 15:50:10 +0530

    implemented node-cron for automatically check deadlinemissed

---
+++ backend/utils/deadlinechecker.js
@@ -0,0 +1,24 @@
+import cron from 'node-cron'
+import Task from '../models/Task.js'
+
+export const startDeadlineChecker = async() => {
+    cron.schedule('0 0 * * *', async() => {
+        try {
+            const today = new Date()
+            const res = await Task.updateMany({
+                deadline : {$lt : today},
+                status : {$ne : 'completed'}
+            },
+            {
+                $set : {status : 'deadlineMissed'}
+            }
+        )
+        console.log(`updated ${res.modifiedCount} tasks`)
+        } catch (error) {
+            console.log('error while updating deadline', error)
+        }
+    }) 
+
+    console.log('deadine checker started')  
+}
+
