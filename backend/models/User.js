8c2f361af20e8d3529685f2d34416cf8e210849f
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-03-21 13:22:03 +0530

    user model imporoved

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,21 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
 
 const userSchema =  mongoose.Schema({
-    name : {type : String,},
-    email : {type : String, unique : true, required : true},
-    password : {type : String, minlength : 6},
+    name : {type : String, required : true},
+    email : {type : String, unique : true, required : true, lowercase : true, trim : true},
+    password : {type : String,required : true,  minlength : 6, select : false},
     role : {type : String, enum:['admin', 'user'], default : 'user'},
     status : {type : String, enum:['active', 'inactive'], default : "active"}
 }, {timestamps : true})
 
-const User = new mongoose.model("User", userSchema)
+userSchema.pre("save", async function(next){
+    if(this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10)
+    }
+    next()
+})
+
+const User = mongoose.model("User", userSchema)
 
 export default User;
