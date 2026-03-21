8075f254366047ee0c92813d77bafd2b50481490
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-03-21 13:28:11 +0530

    default member added in team model

--- backend/models/Team.js
+++ backend/models/Team.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const teamSchema = new mongoose.Schema({
     name : {type : String, required : true},
     description : {type : String},
-    members : [{type : mongoose.Types.ObjectId, ref : "User"}],
+    members : [{type : mongoose.Types.ObjectId, ref : "User", default : []}],
     createdBy : {type : mongoose.Types.ObjectId, ref : "User", required : true},
     status : {type : String, enum : ['active','inactive'], default : 'active'}
 },  {timestamps : true})
